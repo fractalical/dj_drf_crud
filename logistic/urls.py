48443f3f6812dd589c0c5290374ee8833a4d9ddf
Artur <ArthurZ43@yandex.ru>
2023-04-18 19:15:01 +0300

    create deploy pipeline

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,9 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from logistic.views import ProductViewSet, StockViewSet
+from logistic.views import ProductViewSet, StockViewSet, sample_view
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('sample/', sample_view),
+]
+
+urlpatterns += router.urls
--- logistic/views.py
+++ logistic/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -14,3 +16,8 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.get_queryset().order_by('id')
     serializer_class = StockSerializer
     search_fields = ['products__title', 'products__description']
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response({"message": "Hello there"})
