dd3eb6a094718099bcc04fe53d7e5400cfbf2b48
Artur <ArthurZ43@yandex.ru>
2023-04-26 18:25:59 +0300

    change view text

--- logistic/views.py
+++ logistic/views.py
@@ -20,4 +20,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({"message": "Test cicd"})
+    return Response({"message": "Change text from cicd"})
